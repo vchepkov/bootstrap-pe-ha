0f96265eaf9722abdfd5aeaa309cdfc65d7069d3
Vadym Chepkov <vchepkov@gmail.com>
2019-09-15 14:21:08 -0400

    Workaround for bug PE-27066
    - master's catalog can't be compiled by replica

---
+++ Puppetfile
@@ -0,0 +1,6 @@
+#
+# Forge modules
+#
+forge 'http://forge.puppetlabs.com'
+
+mod 'WhatsARanjit/node_manager', :latest
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,14 @@
-# keep PE happy
+# Workaround for bug PE-27066
+# Master's catalog can't be compiled by replica
+if $trusted['certname'] == 'primary.localdomain' {
+  node_group { 'PE Primary Master Agent':
+    ensure  => 'present',
+    classes => {
+      'puppet_enterprise::profile::agent' => {
+        'server_list' => ['primary.localdomain:8140']
+      }
+    },
+    parent  => 'PE Infrastructure Agent',
+    rule    => ['or', ['=', 'name', 'primary.localdomain']],
+  }
+}
