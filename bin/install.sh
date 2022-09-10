f3c5fc83407a9cfd82dd8a04dc01c92fe21e77e8
Vadym Chepkov <vchepkov@gmail.com>
2022-09-10 09:11:13 -0400

    apply pe tunning

--- bin/install.sh
+++ bin/install.sh
@@ -2,4 +2,7 @@
 
 bolt module install --force
 bolt plan run peadm::install --params @install.json
+bolt task run service name=puppet action=stop --no-verbose -t primary.localdomain,replica.localdomain
 bolt command run '/opt/puppetlabs/bin/puppet infrastructure tune --local --pe_conf' -t primary.localdomain
+bolt task run peadm::puppet_runonce --no-verbose -t primary.localdomain
+bolt task run service name=puppet action=start --no-verbose -t primary.localdomain,replica.localdomain
